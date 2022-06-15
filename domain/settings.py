7e6dfe52714c1a93feca282bb777e1fdf79bd13f
santigandolfo <santiago.gandolfo@tiendanube.com>
2022-06-15 18:21:27 -0300

    Generated basic rabbitmq example

--- domain/settings.py
+++ domain/settings.py
@@ -1,8 +1,3 @@
 import os
 
-
-class REDIS:
-    def __init__(self):
-        self.HOST = os.environ['REDIS_HOST']
-        self.PORT = int(os.environ['REDIS_PORT'])
-        self.TIMEOUT = int(os.environ['REDIS_TIMEOUT'])
+BROKER = os.environ['REDIS_HOST']
